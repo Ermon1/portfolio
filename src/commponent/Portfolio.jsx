0f69482e28765a5e03ad0af28e828e09e9488b16
root <root@localhost.localdomain>
2023-12-14 11:57:45 +0300

    final update commit

--- src/commponent/Portfolio.jsx
+++ src/commponent/Portfolio.jsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import carrental from "../asset/carRental.png";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 
 function Portfolio() {
+  const [open, setOpen] = useState(false);
+
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
   const portfolio = [
     {
       id: 1,
       src: carrental,
       chref: "https://github.com/Ermon1/carRental",
       Dhref: "https://github.com/Ermon1/carRental",
+      title: "Car Rental Website",
+      description: `
+    **Introduction:**
+    Welcome to my car rental website plateform, 
+    
+                       **User Roles:**
+    1. **Customer:** Explore our user-friendly platform, create a personalized profile, and effortlessly reserve vehicles with a few clicks. Manage bookings and access transaction history through your dedicated customer dashboard.
+    2. **Renter:** List your vehicle and connect with a broad audience of potential renters. A comprehensive dashboard allows you to manage listings, track bookings, and ensure secure and seamless transactions.
+    3. **Admin:** Oversee and manage the entire platform with powerful tools. Admins play a crucial role in maintaining platform integrity, resolving disputes, and ensuring a secure environment for all users.
+    4. **Employee:** Our dedicated employees contribute to the day-to-day operation, handling customer queries, managing vehicle maintenance, and supporting bookings.
+
+    **Key Features:**
+    - **Profile Management:** Every user role enjoys a dedicated profile page for personal information, preferences, and transaction history.
+    - **Secure Login/Logout:** Robust login/logout functionalities ensure user data protection and easy access.
+    - **Show Car Page:** Explore a comprehensive catalog with detailed vehicle descriptions, images, and rental information.
+    - **Description Page:** Access in-depth information about each vehicle, including specifications, features, and rental terms.
+    - **Booking Processes:** Effortlessly reserve vehicles, update booking details, and manage reservations with user-friendly functionalities.
+    - **Employee Dashboard:** Employees benefit from a centralized dashboard for efficient task management and seamless customer interactions.
+  `,
     },
   ];
   const handleDemo = (href) => {
     window.open(href, "_blank");
-   
   };
   const handleCode = (href) => {
     window.open(href, "_blank");
   };
 
   return (
-    <div
-      name="portfolio"
-      className="w-full text-white    mb-28"
-    >
+    <div name="portfolio" className="w-full text-white    mb-28">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
         <div className="pb-5">
           <p className="text-4xl font-bold inline border-b-4  border-gray-500">
@@ -43,17 +71,39 @@ function Portfolio() {
                 className="rounded-md duration-200 hover:scale-105 "
               />
               <div className="text-center text-slate-950 ">
-                car rental website{" "}
+                <div className="portfolio-item">
+                  <h2>{port.title}</h2>
+
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleToggle}
+                  >
+                    Show Details
+                  </Button>
+
+                  <Dialog open={open} onClose={handleToggle}>
+                    <DialogTitle>{port.title}</DialogTitle>
+                    <DialogContent>
+                      <p className="text-base leading-6 text-gray-700">
+                        {port.description}
+                      </p>
+                    </DialogContent>
+                    <DialogActions>
+                      <Button onClick={handleToggle}>Close</Button>
+                    </DialogActions>
+                  </Dialog>
+                </div>
               </div>
               <div className="flex items-center ml-5 justify-center ">
                 <button
-                  onClick={()=>handleCode(port.chref)}
+                  onClick={() => handleCode(port.chref)}
                   className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
                 >
                   Code
                 </button>
                 <button
-                  onClick={()=>handleDemo(port.Dhref)}
+                  onClick={() => handleDemo(port.Dhref)}
                   className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
                 >
                   Demo
